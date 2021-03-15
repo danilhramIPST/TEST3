23aec7e5c1346ce8dd622459f5537244cb133846
danilhramIPST <danil.hram@yandex.ru>
2021-03-15 18:28:48 +0700

    “Routes_corrected”

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,30 @@
     Route::get('/bank', function () {
         echo 'bank';
     });*/
-//});
-/*php artisan make:controller test\FirstTestController;
-php artisan make:controller test\SecondTestController;*/
-
-
-
-
+//});*/
 
 
 //Route::get('/', [WelcomeController::class, 'show']);
 
-use App\Http\Controllers\WelcomeController;
+/*use App\Http\Controllers\WelcomeController;
 
 Route::group(['prefix' => 'my'], function () {
     Route::get('/controller', [WelcomeController::class, 'get']);
     Route::get('route/{price}', [WelcomeController::class, 'show']);
 
 });
+*/
 
+
+Route::group(['prefix' => 'my'], function () {
+    Route::get('/route/{language?}', function ($language = null) {
+        if ($language) {
+            echo $language;
+        }else {
+    echo 'Language not found,default language - English';
+        }
+
+    });
+
+});
 
